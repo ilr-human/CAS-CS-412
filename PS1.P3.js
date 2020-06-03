d40f543c2efd3d9f8b85e90022ee5bdb9696eaff
BellaRocha <bellailr@bu.edu>
2020-06-02 20:54:30 -0400

    Update PS1.P3.js

--- PS1.P3.js
+++ PS1.P3.js
@@ -3,15 +3,17 @@ const splitter = (str, func) => func(str);
 const lambda1 = (str) => str.split(/(?=c)/g);
 
 const lambda2 = (str) => {
-    const originalString = str;
-    console.log(`originalString: ${originalString}`);
-    const modifiedString = str.replace(/a/g, 'A');
-    console.log(`modifiedString: ${modifiedString}`)
-    const numberReplaced = str.replace(/[^a]/g, "").length;
-    console.log(`numberReplaced: ${numberReplaced}`)
-    const length = str.length;
-    console.log(`length: ${str.length}`)
-    return [originalString, modifiedString, numberReplaced, length];
+    const object = {
+        originalString: str,
+        modifiedString: str.replace(/a/g, 'A'),
+        numberReplaced: str.replace(/[^a]/g, "").length,
+        length: str.length
+    }
+    console.log(`originalString: ${object.originalString}`);
+    console.log(`modifiedString: ${object.modifiedString}`)
+    console.log(`numberReplaced: ${object.numberReplaced}`)
+    console.log(`length: ${object.length}`)
+    return object;
 }
 
 module.exports = {splitter, lambda1, lambda2};
