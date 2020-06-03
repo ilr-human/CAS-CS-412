8d43c13fec62a6f5d6a98a9b2637298d847ff520
BellaRocha <bellailr@bu.edu>
2020-06-02 20:34:04 -0400

    Add files via upload

---
+++ PS1.P3.TEST.js
@@ -0,0 +1,15 @@
+const {describe, it} = require('mocha'); //mocha.js.org
+const {expect} = require('chai');
+
+const {splitter, lambda1, lambda2} = require('./PS1.P3');
+
+describe('PS1.P3 TEST', () => {
+    it(`should return the string split on every 'c' character (including 'c')`, function(){
+        let split1 = splitter('supercalifragilisticexpialidocious', lambda1);
+        expect(split1).to.have.eql(['super','califragilisti','cexpialido','cious']);
+    });
+    it(`should return the originalString, modifiedString, numberReplaces, length`, function(){
+        let split2 = splitter('supercalifragilisticexpialidocious', lambda2);
+        expect(split2).to.have.lengthOf(4);
+    });
+})
