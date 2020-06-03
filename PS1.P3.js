762263b1dbcdfdc449b42f725236d9ef1b90812b
BellaRocha <bellailr@bu.edu>
2020-06-02 20:20:46 -0400

    Update PS1.P3.js

--- PS1.P3.js
+++ PS1.P3.js
@@ -3,7 +3,8 @@ const splitter = (str, func) => func(str);
 const lambda1 = (str) => str.split(/(?=c)/g);
 
 const lambda2 = (str) => {
-    console.log(`originalString: ${str}`);
+    const originalString = str;
+    console.log(`originalString: ${originalString}`);
     const modifiedString = str.replace(/a/g, 'A');
     console.log(`modifiedString: ${modifiedString}`)
     var counter = 0;
@@ -12,8 +13,12 @@ const lambda2 = (str) => {
             counter ++;
         }
     }
-    console.log(`numberReplaced: ${counter}`)
+    const numberReplaced = counter;
+    console.log(`numberReplaced: ${numberReplaced}`)
+    const length = str.length;
     console.log(`length: ${str.length}`)
+    return [originalString, modifiedString, numberReplaced, length];
 }
+
 
 module.exports = {splitter, lambda1, lambda2};
