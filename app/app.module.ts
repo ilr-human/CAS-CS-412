deaaac232949cea8fb1193c6f3dc6bbb79ccf27b
BellaRocha <bellailr@bu.edu>
2020-07-01 21:32:08 -0400

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { CovidComponent } from './covid/covid.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { CovidComponent } from './covid/covid.component';
     CovidComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
