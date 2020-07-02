9b4f595153a0f0cf81023f0ce27eb41b08c1465a
BellaRocha <bellailr@bu.edu>
2020-07-01 21:37:59 -0400

    Update app.component.spec.ts

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PS6'`, () => {
+  it(`should have as title 'PS7'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PS6');
+    expect(app.title).toEqual('PS7');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('PS6 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('PS7 app is running!');
   });
 });
