7e6f92dbbf1eb15aa71cc13a911775fd931eec24
BellaRocha <bellailr@bu.edu>
2020-07-01 21:36:10 -0400

    Update covid-service.service.ts

--- covid/covid-service.service.ts
+++ covid/covid-service.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {FormGroup} from '@angular/forms';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CovidServiceService {
 
+  baseURL: string = "http://corona-api.com/countries/";
+  appid: string = "?appid=a745f6bbabc0e4efa1f96ba24aa5e6db";
+
   constructor(private http: HttpClient) { }
 
-  // tslint:disable-next-line:typedef
+  getStatusByCountry(form: FormGroup){
+    let countryCode = form.value.countryCode;
+    return this.http.get(this.baseURL + countryCode + this.appid);
+  }
+
   getStatus(){
     // returns an observable
     return this.http.get('http://corona-api.com/countries/US?appid=a745f6bbabc0e4efa1f96ba24aa5e6db');
